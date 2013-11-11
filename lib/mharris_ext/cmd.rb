65e8ee98680bccee426a8133162d0ffa55754a36
Mike Harris <mharris717@gmail.com>
2013-11-11 11:53:33 -0500

    cmd

--- lib/mharris_ext/cmd.rb
+++ lib/mharris_ext/cmd.rb
@@ -5,18 +5,24 @@ rescue => exp
   return false
 end
 
-def ec(cmd,ops={})
-  puts cmd unless ops[:silent]
-  res = nil
-  if has_bundler?
-    Bundler.with_clean_env do
+module MharrisExt
+  def self.ec(cmd,ops={})
+    puts cmd unless ops[:silent]
+    res = nil
+    if has_bundler?
+      Bundler.with_clean_env do
+        res = `#{cmd}`
+      end
+    else
       res = `#{cmd}`
     end
-  else
-    res = `#{cmd}`
-  end
 
-  raise "bad cmd #{$?.to_i} #{cmd} #{res}" unless $?.to_i == 0
-  puts res unless ops[:silent]
-  res
+    raise "bad cmd #{$?.to_i} #{cmd} #{res}" unless $?.to_i == 0
+    puts res unless ops[:silent]
+    res
+  end
+end
+
+def ec(*args)
+  MharrisExt.ec(*args)
 end
